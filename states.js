a9daae911278847b5e39242f9cea04684b1c2154
VladislavSo <76833072+VladislavSo@users.noreply.github.com>
2025-09-17 15:53:35 +0200

    Update states.js

--- states.js
+++ states.js
@@ -51,7 +51,7 @@ window.StackUI = window.StackUI || {};
     pageScrollOffsetRem: 6.5,       // Отступ при прокрутке окна к кейсу
 
     // Верхняя полоса влияния (управляет i-1 и i-2)
-    effectStartRem: 10.125,
+    effectStartRem: 10.625,
     effectEndRem: 2.75,
     topIndex1EndRem: -1.5,
     topIndex2StartRem: -1.5,
@@ -59,7 +59,7 @@ window.StackUI = window.StackUI || {};
 
     // Нижняя полоса влияния (управляет i+2 и i+3)
     bottomBandStartRem: 10.625,
-    bottomBandEndRem: 18,
+    bottomBandEndRem: 18.25,
     bottomIndex2StartRem: -1.5,
     bottomIndex2EndRem: 0,
     bottomIndex3StartRem: -2.75,
@@ -119,6 +119,7 @@ window.StackUI = window.StackUI || {};
     cardChildren: []  // Дочерние элементы карточек для управления opacity
   };
 })(window.StackUI);
+
 
 
 
