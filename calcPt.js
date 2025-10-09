1e84ed91e7189c09316838c48322072ad601f00b
VladislavSo <76833072+VladislavSo@users.noreply.github.com>
2025-10-09 14:03:34 +0200

    Update calcPt.js

--- calcPt.js
+++ calcPt.js
@@ -40,7 +40,7 @@
     if (wrapperEl && stackEl) {
       const stackHeightPx = stackEl.getBoundingClientRect().height;
       const wrapperHeightPx = wrapperEl.getBoundingClientRect().height;
-      const marginBottomPx = Math.max(0, Math.round(stackHeightPx - clampedPx - wrapperHeightPx - titlePx - addPx - ns.metrics.root - (ns.metrics.root * 2.75)));
+      const marginBottomPx = Math.max(0, Math.round(stackHeightPx - clampedPx - wrapperHeightPx - titlePx - addPx - ns.metrics.root + (ns.metrics.root * 2.75)));
       console.log(stackHeightPx, clampedPx, wrapperHeightPx, titlePx, addPx, ns.metrics.root);
       wrapperEl.style.marginBottom = `${marginBottomPx}px`;
       console.log(marginBottomPx);
@@ -128,5 +128,6 @@
   ns.layout = ns.layout || {};
   ns.layout.updateCasesContainerPaddingTop = updateCasesContainerPaddingTop;
 })(window.StackUI);
+
 
 
