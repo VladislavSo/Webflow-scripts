96f162aba05c19dcf82ab9eec4746de2728a1d1c
VladislavSo <76833072+VladislavSo@users.noreply.github.com>
2025-11-17 12:07:23 +0100

    Update states.js

--- states.js
+++ states.js
@@ -97,8 +97,7 @@ window.StackUI = window.StackUI || {};
     lastCurrentCard: null,
     removedCurrentCard: null,
     fromListScroll: false,
-    isProgrammaticListScroll: false,
-    settingActiveCase: null
+    isProgrammaticListScroll: false
   };
 
   ns.cache = {
